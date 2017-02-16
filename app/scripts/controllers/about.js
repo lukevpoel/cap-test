e5ce31eb865b41e3198f4ec9e45f2e685cb077bf
Luke Vander Poel <airscout99@gmail.com>
2017-02-15 16:53:54 -0800

    started angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name capTestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the capTestApp
+ */
+angular.module('capTestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name capTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the capTestApp
+ */
+angular.module('capTestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
